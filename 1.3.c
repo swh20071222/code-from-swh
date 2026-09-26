ad2f6e0e17a5035f1b6749257537e90ddba035b9
SWH <15388621206@163.com>
2026-09-26 10:30:01 +0800

    全部完成

--- 1.3.c
+++ 1.3.c
@@ -102,10 +102,10 @@ int main(void)
      first. Ament =3.5*a+0.5*b+6.0*c;
      char filename [100];
      char filetime [100];
-     strftime(filetime, sizeof(filename),
+     strftime(filetime, sizeof(filetime),
              "%Y-%m-%d", localtime(&now));//把具体到天的时间存到filetime中，便于写文件名
      snprintf(filename, sizeof(filename),
-             "C:/Users/15388/Desktop/data_%s.txt", first.time);//将文件名“浓缩”到filename中
+             "C:/Users/15388/Desktop/data_%s.txt", filetime);//将文件名“浓缩”到filename中
     
   FILE *fp = fopen(filename, "at");
             
@@ -117,19 +117,61 @@ int main(void)
             else
             
       {
-        fprintf(fp,"No.   Time      Items              Ament\n ");
+        fprintf(fp,"No.   Time                                               Items               Ament\n ");
         fprintf(fp,"-------------------------------------------\n");
-        fprintf(fp,"1,    %s,      colla,%d\n",first . time,first.cola);
-        fprintf(fp,"                 Lollipop,%d\n",first.Lollipop);
-        fprintf(fp,"                 Noodles,%d,       %.2f\n",first.Noodles,first. Ament);
+        fprintf(fp,"1,    %s,                   colla,%d\n",first . time,first.cola);
+        fprintf(fp,"                                                               Lollipop,%d\n",first.Lollipop);
+        fprintf(fp,"                                                               Noodles,%d,       %.2f\n",first.Noodles,first. Ament);
         fclose(fp);
       }
   a=0;b=0;c=0;
  
-  
-  
-
-   }
+  }
+else if(strcmp(number,"sales")==0)  //先读取sales得到指令
+ {  char day[100];//再建立字符串数组用于储存sales后面的日期
+    int duqu=getchar();//再向后读取一个字符并赋给duqu
+    if(duqu==' ')//读到空格说明用户输入了日期
+       {
+        scanf("%99s",day);
+       }//把日期存储到day数组
+    else{
+        day[0] = '\0';//没有日期不存储
+    }
+    if (strlen(day) == 0) //如果没有输入日期
+         {
+                time_t now = time(NULL);//调取现在时间
+                
+                strftime(day, sizeof(day), "%Y-%m-%d", localtime(&now));//存储到day中
+         }
+            char chazhao[100];
+            sprintf(chazhao, "C:/Users/15388/Desktop/data_%s.txt", day);//把要查的文件名存储到chazhao中
+            FILE *fp = fopen(chazhao, "r");//打开需要的文件
+            float jine = 0.0 ;
+            float daily = 0.0;
+            int hangshu = 0 ;
+           if (fp != NULL)
+           {
+            char hang[200];
+        while (fgets(hang, sizeof(hang), fp))//逐行读取文件并存入hang，直到读完结束循环
+           {               hangshu++;//每读一行hangshu+1
+           printf("%s", hang);//原样打印一天的销售记录
+          if (hangshu == 5)//第五行存在总金额
+          {
+            sscanf(hang, "%*[^,],%*d,%f", &jine);//跳过非逗号字符，逗号，整数，逗号，然后读浮点数
+            daily += jine;
+            hangshu = 0;
+             
+           }
+           }
+        fclose(fp);
+        }
+        printf("--------------------------------\n");
+        printf("Daily: %.2f\n", daily);
+}
+else if(strcmp(number,"newday")==0)
+{
+    printf("New day started. Today's sales records cleared.\n");
+}//由于已经通过文件名实现了要求3，所以只需要得到命令后给用户一个反馈就可以了
 
 }
 }
